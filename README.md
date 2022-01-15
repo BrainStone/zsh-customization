9d52dabd6c916a2eb6e3e23959e7ea6486668867
BrainStone <yannick@tedworld.de>
2022-01-15 01:09:32 +0100

    Further improvements on the installation script
    Also we need to keep stdin of sh free, so we can't use the pipe method

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ These are my zsh settings and customizations. Will probably spend waaaaay too mu
 
 Installing this is super straight forward!
 
-Just run the install.sh: `curl -fsSL https://raw.githubusercontent.com/BrainStone/zsh-customization/master/install.sh | sh`
+Just run the install.sh: `sh -c "$(curl -fsSL https://raw.githubusercontent.com/BrainStone/zsh-customization/master/install.sh)"`
--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-export CUSTOMIZATION_DIR="${CUSTOMIZATION_DIR:-"${HOME}/zsh-customization"}"
+# Set installation dir (may be customized externally)
+CUSTOMIZATION_DIR="${CUSTOMIZATION_DIR:-"${HOME}/zsh-customization"}"
 
+# Download or update git repo
 if [ ! -d "$CUSTOMIZATION_DIR" ]; then
   git clone --recursive https://github.com/BrainStone/zsh-customization.git "$CUSTOMIZATION_DIR"
 else
@@ -9,14 +11,17 @@ else
   git -C "$CUSTOMIZATION_DIR" submodule update --init
 fi
 
+# Take care of existing .zshrc
 [ -f "${HOME}/.zshrc" ] &&
   [ "$(head -n1 "${HOME}/.zshrc")" != "$(head -n1 "${CUSTOMIZATION_DIR}/root_zshrc.zsh")" ] &&
   mv "${HOME}/.zshrc" "${HOME}/.zshrc.orig"
 
+# Install new zshrc
 sed "s@XXX_PATH_XXX@${CUSTOMIZATION_DIR}@g" "${CUSTOMIZATION_DIR}/root_zshrc.zsh" >"${HOME}/.zshrc"
 
+# Get rid of existing Oh My ZSH installation
 OHMYZSH="${ZSH:-"${HOME}/.oh-my-zsh"}"
-[ "$OHMYZSH" == "$CUSTOMIZATION_DIR" ] && OHMYZSH="${HOME}/.oh-my-zsh"
+[ "$OHMYZSH" == "${CUSTOMIZATION_DIR/oh-my-zsh}" ] && OHMYZSH="${HOME}/.oh-my-zsh"
 
 if [ -d "$OHMYZSH" ]; then
   echo "Found existing \"Oh My ZSH\" installation in \"${OHMYZSH}\"!"
@@ -37,3 +42,6 @@ if [ -d "$OHMYZSH" ]; then
     esac
   done
 fi
+
+# Run zsh or just update it if already running!
+exec zsh
