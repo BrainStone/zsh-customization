734b0fbdb0e4ffb3d6637a00c45aa74d849da897
BrainStone <yannick@tedworld.de>
2022-01-15 19:08:13 +0100

    Using the proper variable name
    This allows the install script to still use the proper location in case of a non standard dir

--- install.sh
+++ install.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 
 # Set installation dir (may be customized externally)
-CUSTOMIZATION_DIR="${CUSTOMIZATION_DIR:-"${HOME}/zsh-customization"}"
+ZSH_CUSTOMIZATION_BASE="${ZSH_CUSTOMIZATION_BASE:-"${HOME}/zsh-customization"}"
 
 # Download or update git repo
-if [ ! -d "$CUSTOMIZATION_DIR" ]; then
-  git clone --recursive --jobs=10 https://github.com/BrainStone/zsh-customization.git "$CUSTOMIZATION_DIR"
+if [ ! -d "$ZSH_CUSTOMIZATION_BASE" ]; then
+  git clone --recursive --jobs=10 https://github.com/BrainStone/zsh-customization.git "$ZSH_CUSTOMIZATION_BASE"
 else
-  git -C "$CUSTOMIZATION_DIR" pull --recurse-submodules --jobs=10
+  git -C "$ZSH_CUSTOMIZATION_BASE" pull --recurse-submodules --jobs=10
 fi
 
 # Take care of existing .zshrc
 [ -f "${HOME}/.zshrc" ] &&
-  [ "$(head -n1 "${HOME}/.zshrc")" != "$(head -n1 "${CUSTOMIZATION_DIR}/root_zshrc.zsh")" ] &&
+  [ "$(head -n1 "${HOME}/.zshrc")" != "$(head -n1 "${ZSH_CUSTOMIZATION_BASE}/root_zshrc.zsh")" ] &&
   mv "${HOME}/.zshrc" "${HOME}/.zshrc.orig"
 
 # Install new zshrc
-sed "s@XXX_PATH_XXX@${CUSTOMIZATION_DIR}@g" "${CUSTOMIZATION_DIR}/root_zshrc.zsh" >"${HOME}/.zshrc"
+sed "s@XXX_PATH_XXX@${ZSH_CUSTOMIZATION_BASE}@g" "${ZSH_CUSTOMIZATION_BASE}/root_zshrc.zsh" >"${HOME}/.zshrc"
 
 # Get rid of existing Oh My ZSH installation
 OHMYZSH="${ZSH:-"${HOME}/.oh-my-zsh"}"
-[ "$OHMYZSH" = "${CUSTOMIZATION_DIR}/oh-my-zsh" ] && OHMYZSH="${HOME}/.oh-my-zsh"
+[ "$OHMYZSH" = "${ZSH_CUSTOMIZATION_BASE}/oh-my-zsh" ] && OHMYZSH="${HOME}/.oh-my-zsh"
 
 if [ -d "$OHMYZSH" ]; then
   echo "Found existing \"Oh My ZSH\" installation in \"${OHMYZSH}\"!"
