42bbc9c577c8025684c4c42cb3444d4a5007adaf
BrainStone <yannick@tedworld.de>
2022-01-15 02:56:29 +0100

    Removed unused comment

--- zshrc.zsh
+++ zshrc.zsh
@@ -85,9 +85,6 @@ ZSH_THEME="powerlevel10k/powerlevel10k"
 # see 'man strftime' for details.
 # HIST_STAMPS="mm/dd/yyyy"
 
-# Would you like to use another custom folder than $ZSH/custom?
-# ZSH_CUSTOM=/path/to/new-custom-folder
-
 # Which plugins would you like to load?
 # Standard plugins can be found in $ZSH/plugins/
 # Custom plugins may be added to $ZSH_CUSTOM/plugins/
