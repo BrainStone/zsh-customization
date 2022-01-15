02ba12ba31719c3beb6ad3ed5f4a62cb9afa2668
BrainStone <yannick@tedworld.de>
2022-01-15 01:50:12 +0100

    Added powerlevel10k theme and adjusted bash zshrc for it. No theme config yet!

--- zshrc.zsh
+++ zshrc.zsh
@@ -1,15 +1,23 @@
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
 # If you come from bash you might have to change your $PATH.
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 
 # Path to your oh-my-zsh installation.
-export ZSH="${0:a:h}/oh-my-zsh"
-export ZSH_CUSTOM="${0:a:h}/custom"
+export ZSH_CUSTOMIZATION_BASE="${0:a:h}"
+export ZSH="${ZSH_CUSTOMIZATION_BASE}/oh-my-zsh"
+export ZSH_CUSTOM="${ZSH_CUSTOMIZATION_BASE}/custom"
 
 # Set name of the theme to load --- if set to "random", it will
 # load a random theme each time oh-my-zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
-ZSH_THEME="robbyrussell"
+ZSH_THEME="powerlevel10k/powerlevel10k"
 
 # Set list of themes to pick from when loading at random
 # Setting this variable when ZSH_THEME=random will cause zsh to load
@@ -76,3 +84,6 @@ plugins=(command-not-found git zsh-autosuggestions zsh-syntax-highlighting)
 source $ZSH/oh-my-zsh.sh
 
 # User configuration
+
+# To customize prompt, run `p10k configure` or edit "${ZSH_CUSTOMIZATION_BASE}/p10k.zsh".
+[[ -f "${ZSH_CUSTOMIZATION_BASE}/p10k.zsh" ]] && source "${ZSH_CUSTOMIZATION_BASE}/p10k.zsh"
