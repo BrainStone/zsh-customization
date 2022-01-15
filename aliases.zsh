3928ed95a25415afbf2bd84f3f32295bb8b1431f
BrainStone <yannick@tedworld.de>
2022-01-15 18:50:59 +0100

    Adding selfupdate call

--- aliases.zsh
+++ aliases.zsh
@@ -1,6 +1,9 @@
 # Just some useful aliases I picked up over the years.
 # Pretty sure this list will grow and grow!
 
+# Selfupdate!
+alias update-zsh-theme='sh "${ZSH_CUSTOMIZATION_BASE}/install.sh"'
+
 # Generic stuff
 alias ,='sudo -i'
 alias ll='ls -lAh'
