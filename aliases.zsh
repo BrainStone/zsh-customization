9bd12fbbdb4999a13eed673b9fd2f16062cf09ff
BrainStone <yannick@tedworld.de>
2022-01-15 20:35:06 +0100

    Properly define update method depending on installation type

--- aliases.zsh
+++ aliases.zsh
@@ -1,8 +1,28 @@
 # Just some useful aliases I picked up over the years.
 # Pretty sure this list will grow and grow!
 
+# Helper stuff
+check_sudo() {
+  # We are root, no need to check.
+  [ "$EUID" -eq 0 ] && return 0
+
+  local prompt
+  prompt=$(sudo -nv 2>&1)
+  if [ $? -eq 0 ]; then
+    return 0 # Has sudo permissions and password entered recently
+  elif echo $prompt | grep -q '^sudo:'; then
+    return 0 # Has sudo permissions but needs password
+  else
+    return 1 # No sudo permissions whatsoever
+  fi
+}
+
 # Selfupdate!
-alias update-zsh-theme='sh "${ZSH_CUSTOMIZATION_BASE}/install.sh" --skip-restart; exec zsh'
+if [[ "$ZSH_INSTALL_GLOBALLY" == "true" ]] && check_sudo; then
+  alias update-zsh-theme='sudo HOME="$HOME" sh "${ZSH_CUSTOMIZATION_BASE}/install.sh" --skip-restart; exec zsh'
+else
+  alias update-zsh-theme='sh "${ZSH_CUSTOMIZATION_BASE}/install.sh" --skip-restart; exec zsh'
+fi
 
 # Generic stuff
 alias ,='sudo -i'
