d9c269f662fe913cb1087ec366ffc3738f6941fd
Yannick Schinko <yannick@tedworld.de>
2025-01-20 10:43:59 +0100

    Invert sourcing logic
    This is done to ensure the commands are always successful. Primarily so that when starting the shell it doesn't start with exit code 1.
    
    Either the file isn't found (sucess) or it's found and sourced (also success).

--- zshrc/root_zshrc.zsh
+++ zshrc/root_zshrc.zsh
@@ -2,11 +2,11 @@
 # Please do not customize this file, but instead use either your ~/.zshrc_local or
 # ~/.zshrc_local_post files! (Create them if they don't exist)
 
-[ -f ~/.zshrc_local ] && source ~/.zshrc_local
+[ ! -f ~/.zshrc_local ] || source ~/.zshrc_local
 
 # Are we using the global config?
 export ZSH_INSTALL_GLOBALLY=XXX_GLOBAL_XXX
 # The actual .zshrc file. Contains all centralized customizations!
 source "XXX_PATH_XXX/zshrc/zshrc.zsh"
 
-[ -f ~/.zshrc_local_post ] && source ~/.zshrc_local_post
+[ ! -f ~/.zshrc_local_post ] || source ~/.zshrc_local_post
